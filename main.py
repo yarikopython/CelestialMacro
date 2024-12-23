7e142212ecd47630c8f796e6ff6cf5a5ad7fd5e0
yarikopython <kozoderovaroslav@gmail.com>
2024-12-23 19:41:21 +0100

    Some basics

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import discord
 from discord.ext import commands
 from tokens import discord_token
+from time import sleep
+from ahk import AHK
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
 
+ahk = AHK()
+
+
+class Macro:
+    
+    def quest(self):
+        pass
+
+    def equip(self, aura):
+        pass
+    
+    def use(self, item):
+        pass
