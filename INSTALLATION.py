f6a4b5107c916b72ac656573caa1c5c3b327f3db
yarikopython <kozoderovaroslav@gmail.com>
2024-12-30 20:34:50 +0100

    deleted installation.py

--- INSTALLATION.py
+++
@@ -1,8 +0,0 @@
-import sys
-import subprocess
-
-def install_requirements():
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-    print("requirements has been installed.")
-
-install_requirements()
